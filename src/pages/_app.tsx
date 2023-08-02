21e389ffd337423d133ba8b104f6b58f8c310b47
449sabu <449sabu@gmail.com>
2023-08-02 12:47:00 +0900

    Add library Mantine etc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,36 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light',
+          globalStyles: (theme) => ({
+            body: {
+              backgroundColor: theme.colorScheme === 'dark' ? "rgb(43,47,55)" : "#F0F0F0",
+            },
+            ".fontNm": {
+              color: "#F0F0F0",
+              textShadow: "6px 6px 12px #dadada, -6px -6px 12px #ffffff"
+            }
+          })
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
